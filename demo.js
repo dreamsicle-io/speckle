2a761908b364c21f7b3813044a99648f022c3c94
Noah Mason <noahmason23@gmail.com>
2018-02-27 13:44:00 -0500

    Wraps demo speckle calls in DOMContentLoaded event listener.

--- demo.js
+++ demo.js
@@ -1,14 +1,16 @@
 'use-strict';
 
-new Speckle(document.querySelector('#default'));
-new Speckle(document.querySelector('#large'));
-new Speckle(document.querySelector('#small'));
-new Speckle(document.querySelector('#mono'));
-new Speckle(document.querySelector('#alot'));
-new Speckle(document.querySelector('#alittle'));
-new Speckle(document.querySelector('#deco'));
-new Speckle(document.querySelector('#crop'));
-new Speckle(document.querySelector('#bokeh'));
-new Speckle(document.querySelector('#multiple'));
-new Speckle(document.querySelector('#multiple'));
-new Speckle(document.querySelector('#multiple'));
+document.addEventListener('DOMContentLoaded', function(e) {
+	new Speckle(document.querySelector('#default'));
+	new Speckle(document.querySelector('#large'));
+	new Speckle(document.querySelector('#small'));
+	new Speckle(document.querySelector('#mono'));
+	new Speckle(document.querySelector('#alot'));
+	new Speckle(document.querySelector('#alittle'));
+	new Speckle(document.querySelector('#deco'));
+	new Speckle(document.querySelector('#crop'));
+	new Speckle(document.querySelector('#bokeh'));
+	new Speckle(document.querySelector('#multiple'));
+	new Speckle(document.querySelector('#multiple'));
+	new Speckle(document.querySelector('#multiple'));
+});

f1aeaa1a819c9f908b8912b97f9393196f29f9bf
Noah Mason <noahmason23@gmail.com>
2018-02-27 03:25:50 -0500

    Adds all initializations for Demo.

--- demo.js
+++ demo.js
@@ -1,3 +1,14 @@
 'use-strict';
 
-const speckleDefault = new Speckle(document.querySelector('#default'));
+new Speckle(document.querySelector('#default'));
+new Speckle(document.querySelector('#large'));
+new Speckle(document.querySelector('#small'));
+new Speckle(document.querySelector('#mono'));
+new Speckle(document.querySelector('#alot'));
+new Speckle(document.querySelector('#alittle'));
+new Speckle(document.querySelector('#deco'));
+new Speckle(document.querySelector('#crop'));
+new Speckle(document.querySelector('#bokeh'));
+new Speckle(document.querySelector('#multiple'));
+new Speckle(document.querySelector('#multiple'));
+new Speckle(document.querySelector('#multiple'));
